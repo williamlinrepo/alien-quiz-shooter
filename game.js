f6372cc84e3f42feb6c934c95e710564237bbe13
George Lin <zhujin.lin@gmail.com>
2026-05-17 16:27:01 +0100

    feat: title scene with pack picker

--- game.js
+++ game.js
@@ -17,13 +17,77 @@ async function loadPacks() {
   return packs;
 }
 
-scene("title", () => {
+function defaultUpgrades() {
+  return {
+    lives: 1,
+    shieldHits: 0,
+    weapon: "blaster",   // "blaster" | "spread" | "laser"
+    smartBombs: 0,
+    scoreMultiplier: 1,
+  };
+}
+
+scene("title", (packs) => {
   add([
-    text("ALIEN QUIZ SHOOTER", { size: 32 }),
-    pos(width() / 2, height() / 2),
+    text("ALIEN QUIZ SHOOTER", { size: 40, font: "monospace" }),
+    pos(width() / 2, 80),
     anchor("center"),
-    color(255, 255, 0),
+    color(255, 230, 0),
   ]);
+
+  add([
+    text("Choose a topic pack:", { size: 20 }),
+    pos(width() / 2, 160),
+    anchor("center"),
+    color(200, 200, 255),
+  ]);
+
+  packs.forEach((pack, i) => {
+    const btnY = 230 + i * 80;
+    const btn = add([
+      rect(400, 60, { radius: 8 }),
+      pos(width() / 2, btnY),
+      anchor("center"),
+      color(30, 30, 80),
+      area(),
+      "packBtn",
+    ]);
+
+    add([
+      text(pack.name, { size: 22 }),
+      pos(width() / 2, btnY - 8),
+      anchor("center"),
+      color(255, 255, 255),
+    ]);
+
+    add([
+      text(pack.description, { size: 13 }),
+      pos(width() / 2, btnY + 16),
+      anchor("center"),
+      color(160, 160, 200),
+    ]);
+
+    btn.onClick(() => {
+      go("quiz", { pack, questionIndex: 0, upgrades: defaultUpgrades() });
+    });
+  });
+
+  add([
+    text("High Scores", { size: 18 }),
+    pos(width() / 2, height() - 40),
+    anchor("center"),
+    color(100, 200, 100),
+    area(),
+  ]).onClick(() => go("highscores"));
 });
 
-go("title");
+scene("quiz", (data) => {
+  add([text("QUIZ (stub)"), pos(width()/2, height()/2), anchor("center")]);
+});
+
+scene("highscores", () => {
+  add([text("HIGH SCORES (stub)"), pos(width()/2, height()/2), anchor("center")]);
+  onKeyPress("escape", () => loadPacks().then((packs) => go("title", packs)));
+});
+
+loadPacks().then((packs) => go("title", packs));
